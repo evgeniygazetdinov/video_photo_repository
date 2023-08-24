c0e58f4edbd46279fd8137c0cbc46f9f562f5775
evgeniygazetdinov <zplacesound@gmail.com>
2023-08-24 09:17:40 +0300

    add parameters of request

--- main.go
+++ main.go
@@ -14,56 +14,37 @@ import (
 
 
 
-func succes_place(w http.ResponseWriter, r *http.Request) {
+func jsonResponse(forJsonCondition string, w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(map[string]string{"respose":"1","Status": "true", "Err": ""})
+	json.NewEncoder(w).Encode(map[string]string{"respose":forJsonCondition,"Status": "true", "Err": ""})
 }
 
 
-// func get_variable_string_from_uri(variable_name string, w http.ResponseWriter, r *http.Request) []string {
-// 	variable, ok := r.URL.Query()[variable_name]
-// 	if !ok || len(variable[0]) < 1 {
-// 		w.Header().Set("Content-Type", "application/json")
-// 		w.Write([]byte(`{"message": "missed"}`))
-// 	}
-// 	return variable
-// }
-
-// func get_integers_for_equalize(one_name string, second_name string, w http.ResponseWriter, r *http.Request) (int, int) {
-
-// 	one := get_variable_string_from_uri(one_name, w, r)
-// 	two := get_variable_string_from_uri(second_name, w, r)
-// 	first_int, err := strconv.Atoi(one[0])
-// 	second_int, err := strconv.Atoi(two[0])
-// 	if err != nil {
-// 		fmt.Println((err))
-// 	}
-// 	return first_int, second_int
-// }
-
-
-
-func get(w http.ResponseWriter, r *http.Request) {
-	// first, second := get_integers_for_equalize(one_predicate, two_predicate, w, r)
-	// result := first + second
-	succes_place(w, r)
+func get_variable_string_from_uri(variable_name string, w http.ResponseWriter, r *http.Request) string {
+	variable := r.URL.Query().Get(variable_name)
+	if len(variable) < 0 {
+		w.Header().Set("Content-Type", "application/json")
+		w.Write([]byte(`{"message": "missed"}`))
+	}
+	return variable
 }
 
 func upload(w http.ResponseWriter, r *http.Request) {
-	// first, second := get_integers_for_equalize(one_predicate, two_predicate, w, r)
-	// result := first + second
-	succes_place(w, r)
+	jsonResponse("1", w, r)
+}
+
+func downloadVideo(writter http.ResponseWriter, request *http.Request){
+	videoId := get_variable_string_from_uri("id", writter, request)
+	jsonResponse(videoId, writter, request)
 }
 
 
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/api/upload/", upload)
-	router.HandleFunc("/api/download/", download)
-	router.HandleFunc("/api/stream_video/", download)
-	router.HandleFunc("/api/video_list/", download)
-	router.HandleFunc("/api/photo_list/", download)
+	router.HandleFunc("/api/download_by_id/", downloadVideo)
+
 
 	loggedRouter := handlers.LoggingHandler(os.Stdout, router)
 	log.Println("listening on 8081")
